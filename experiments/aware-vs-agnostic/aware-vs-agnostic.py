722128fa9b0ae909b57aeddfa23eec2980cff859
Jamshid Mozafari <93037500+JamshidJDMY@users.noreply.github.com>
2026-06-27 15:40:04 +0200

    Update aware-vs-agnostic.py

--- experiments/aware-vs-agnostic/aware-vs-agnostic.py
+++ experiments/aware-vs-agnostic/aware-vs-agnostic.py
@@ -4,43 +4,6 @@ from statistics import mean
 from prettytable import PrettyTable
 from draw_chart import draw_chart
 
-# 'relevance-contextual-roberta-large',
-# 'relevance-rouge1',
-# 'relevance-rouge2',
-# 'relevance-rougeL',
-# 'relevance-non-contextual-6B-trf',
-# 'relevance-non-contextual-42B-trf',
-# 'relevance-contextual-bert-base',
-# 'relevance-llm-meta-llama_Meta-Llama-3.1-8B-Instruct-Turbo',
-#
-# 'readability-flesch_kincaid_reading_ease-trf',
-# 'readability-gunning_fog_index-trf',
-# 'readability-smog_index-trf',
-# 'readability-coleman_liau_index-trf',
-# 'readability-automated_readability_index-trf',
-# 'readability-ml-xgboost-trf',
-# 'readability-ml-random_forest-trf',
-# 'readability-nn-bert-base',
-# 'readability-nn-roberta-large',
-# 'readability-llm-meta-llama_Meta-Llama-3.1-8B-Instruct-Turbo',
-#
-# 'convergence-specificity-bert-base',
-# 'convergence-specificity-roberta-large',
-# 'convergence-nn-bert-base',
-# 'convergence-nn-roberta-large',
-# 'convergence-llm-llama-3-70b'
-
-# 'familiarity-freq-include_stop_words-trf',
-# 'familiarity-freq-exclude_stop_words-trf',
-# 'familiarity-wikipedia-trf',
-
-# 'answer-leakage-lexical-include_stop_words-trf',
-# 'answer-leakage-lexical-exclude_stop_words-trf',
-# 'answer-leakage-contextual-include_stop_words-trf',
-# 'answer-leakage-contextual-exclude_stop_words-trf',
-
-
-
 def evaluate(subset: Subset):
     results = {'relevance': [], 'readability': [], 'convergence': [], 'familiarity': [], 'answer-leakage': []}
     instances = subset.get_instances()
